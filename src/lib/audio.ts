8c5e230a7125a6abc50e21e9d1c6142d721aa612
faithleysath <faithleysath@gmail.com>
2025-10-31 10:54:56 +0800

    Revert "wip"
    This reverts commit 5882064dd77547230723d963eabb376e64eedd64.

--- src/lib/audio.ts
+++ src/lib/audio.ts
@@ -35,7 +35,7 @@ async function initializeAudio() {
   await Tone.start();
   reverb = new Tone.Reverb(0.7).toDestination();
   polySynth = new Tone.PolySynth(Tone.Synth, {
-    volume: 20,
+    volume: 10,
   }).connect(reverb);
   audioInitialized = true;
   console.log('Audio context started and initialized.');
